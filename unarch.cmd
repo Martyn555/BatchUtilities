2b5e36fc5680e3d12c8518e6673544a8ae7b4e59
Martyn <66063147+Martyn555@users.noreply.github.com>
2021-01-27 02:28:06 +0100

    Add files via upload

---
+++ unarch.cmd
@@ -0,0 +1,11 @@
+@echo off
+rem Martyn's Studio
+if %1==help goto help
+call powershell Expand-Archive -Force %1 %2
+exit /b
+
+:help
+echo unarch ["archive path"] ["output folder"]
+echo.
+echo Unarchive allows you to easily extract files from an archive.
+pause
