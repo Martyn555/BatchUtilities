583330b64e2e1ab1b17d02323ad90eb482389184
Martyn <66063147+Martyn555@users.noreply.github.com>
2021-01-27 02:54:49 +0100

    Add files via upload

---
+++ arch.cmd
@@ -0,0 +1,12 @@
+@echo off
+rem Martyn's Studio
+if %1== help goto help
+if not exist %2 call powershell Compress-Archive -Path %1 -DestinationPath %2
+if exist %2 call powershell Compress-Archive -Path %1 -Update -DestinationPath %2
+exit /b
+
+:help
+echo unarch ["file path"] ["output archive"]
+echo.
+echo It allows you to add a file to the archive or create an archive with the selected file.
+pause
